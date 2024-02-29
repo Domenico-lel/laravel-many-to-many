0b5e1d087f9ecd8ba5731664cf7fd5f3bd4f429b
Domenico-lel <domenicolella2@gmail.com>
2024-02-29 10:07:59 +0100

    set upp front end

---
+++ front-end/vuejs-front-end/src/technology.vue
@@ -0,0 +1,27 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Technology',
+
+  data() {
+    return {
+      technologies: []
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:3000/v1/technologies')
+      .then(response => {
+        this.technologies = response.data
+      })
+  }
+}
+</script>
+
+<template>
+  <h1>hello, word</h1>
+</template>
+
+<style scoped>
+
+</style>
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\ProjectController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+route::group(['prefix' => 'projects'], function () {
+    Route::get('/', [ProjectController::class, 'index']);
+    Route::post('/', [ProjectController::class, 'store']);
+    Route::get('/{project}', [ProjectController::class, 'show']);
+    Route::put('/{project}', [ProjectController::class, 'update']);
+    Route::delete('/{project}', [ProjectController::class, 'destroy']);
 });
